187dbb92d1d4ce6fa20f7376cc96ebd82001a1ce
xerobyte <psurrellr@gmail.com>
2017-01-22 18:50:47 +0100

    more bugfixes

--- src/Gardenerplayer/Communication.java
+++ src/Gardenerplayer/Communication.java
@@ -1,10 +1,49 @@
 package Gardenerplayer;
 
+import battlecode.common.RobotController;
+
 /**
  * Created by Ivan on 1/15/2017.
  */
 public class Communication {
 
+    //NOU
+    /*Types of message*/
+    static final int ENEMYCHANNEL = 1000;
+    static final int STOPCHANNEL = 1100;
+    static final int ENEMYGARDENERCHANNEL = 1200;
+    static final int CHOPCHANNEL = 1300;
+    static final int ENEMYTREECHANNEL = 1400;
+    static final int EMERGENCYCHANNEL = 1500;
+    static final int TREEWITHGOODIES = 1600;
+    static final int PLANTTREECHANNEL = 1700;
+
+    static final int CYCLIC_CHANNEL_LENGTH = 99;
+
+    static int xBase, yBase;
+
+
+    //BC parameters
+
+    static final int[] unitChannels = {501, 502, 503, 504, 505, 506};
+    static final int INITIALIZED = 507;
+
+    static final int ARCHONTURN = 508;
+    static final int ARCHONNUMBER = 509;
+
+    static final int MAX_BROADCAST_MESSAGE = 500;
+
+    static final int typeMask = 0xF0000000; //at most 15
+    static final int iOffMask = 0x0FF00000; //at most 255
+    static final int jOffMask = 0x000FF000; //at most 255
+    static final int iOffShift = 20;
+    static final int jOffShift = 12;
+    static final int valueMask = 0x00000FFF; // at most 4095
+
+
+
+
+    //VELL
     /*Types of message*/
 
     static final int STOP = 0x10000000; //tells ally soldier to stop moving (collision)
@@ -16,39 +55,27 @@ public class Communication {
 
 
 
-    //BC parameters
-
-    static final int[] unitChannels = {501, 502, 503, 504, 505, 506};
-    static final int INITIALIZED = 507;
-
-    static final int ARCHONTURN = 508;
-    static final int ARCHONNUMBER = 509;
-
     // ocupa de la 600 a la 614 teoricament
     // tinc 18*18 zones => 324
     // cada zone ocupa 5 bits => 6 zones per int
     // necessito 54 channels
-
+    //a la practica em sembla que ocupo del 600 al 685 o algo aixi xd
     static final int ZONE_FIRST_POSITION = 600;
-    static final int ZONE_CHANNELS = 60;
-
-    static final int UNITS_BUILT = 690;
+    static final int ROBOTS_BUILT = 695;
+    static final int TREES_BUILT = 696;
 
     static int MIN_ZONE_X = 691;//tenen els valors de les zones desfasats +-20 per tal que el 0 mai caigui entre minim i maxim
-    static int MIN_ZONE_Y = 692;
-    static int MAX_ZONE_X = 693;
+    static int MIN_ZONE_Y = 692;//osigui si la x de zona maxima es 6, aqui es guardara un -14
+    static int MAX_ZONE_X = 693;//i si la de zona minima es -3 aqui es guardara un 17
     static int MAX_ZONE_Y = 694;
     static int ZONE_LIMIT_OFFSET = 20;
 
 
-    static final int MAX_BROADCAST_MESSAGE = 500;
 
-    static final int typeMask = 0xF0000000; //at most 15
-    static final int iOffMask = 0x0FF00000; //at most 255
-    static final int jOffMask = 0x000FF000; //at most 255
-    static final int iOffShift = 20;
-    static final int jOffShift = 12;
-    static final int valueMask = 0x00000FFF; // at most 4095
+    static void setBase(int x, int y){
+        xBase = x;
+        yBase = y;
+    }
 
 
     public static int encodeFinding(int type, int iOffset, int jOffset, int value) {
@@ -70,6 +97,19 @@ public class Communication {
         ret[2] = ((bitmap & jOffMask) >> jOffShift) - 127;
         ret[3] = (bitmap & valueMask);
         return ret;
+    }
+
+
+    public static void sendMessage(RobotController rc, int channel, int x, int y, int value) {
+        try {
+            int lastMessage = rc.readBroadcast(channel + CYCLIC_CHANNEL_LENGTH);
+            int message = encodeFinding(0, x-xBase, y-yBase, value);
+            rc.broadcast(channel + lastMessage, message);
+            rc.broadcast(channel + CYCLIC_CHANNEL_LENGTH, (lastMessage + 1) % CYCLIC_CHANNEL_LENGTH);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
     }
 
 }
