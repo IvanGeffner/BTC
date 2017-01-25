2e7ff83c04d08c9cf71dc00cea7cb8cffd120f9d
IvanGeffner <ivanegeffner@gmail.com>
2017-01-25 13:56:45 -0500

    bytecode adjust

--- src/MergedplayerMicro/Shake.java
+++ src/MergedplayerMicro/Shake.java
@@ -17,7 +17,7 @@ public class Shake {
 
         for (TreeInfo ti : Ti){
             if (ti.getContainedBullets() > maxBullets){
-                if (!rc.canShake(ti.getID())) continue;
+                if (!rc.canShake(ti.getID())) break;
                 maxBullets = ti.getContainedBullets();
                 id = ti.getID();
             }
