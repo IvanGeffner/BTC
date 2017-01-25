8bc8a78d0a5b23505289efe0c52af7025b29eee7
IvanGeffner <ivanegeffner@gmail.com>
2017-01-25 16:51:42 -0500

    ewrwerw

---
+++ src/Mergedplayermicro2/Shake.java
@@ -0,0 +1,33 @@
+package Mergedplayermicro2;
+
+import battlecode.common.RobotController;
+import battlecode.common.Team;
+import battlecode.common.TreeInfo;
+
+/**
+ * Created by Pau on 24/01/2017.
+ */
+public class Shake {
+
+    static void shake(RobotController rc){
+
+        float maxBullets = 0;
+        int id = -1;
+        TreeInfo[] Ti = rc.senseNearbyTrees(rc.getType().bodyRadius + 1.0f, Team.NEUTRAL);
+
+        for (TreeInfo ti : Ti){
+            if (ti.getContainedBullets() > maxBullets){
+                if (!rc.canShake(ti.getID())) break;
+                maxBullets = ti.getContainedBullets();
+                id = ti.getID();
+            }
+        }
+        try {
+            if (maxBullets > 0) rc.shake(id);
+            else return;
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+            e.printStackTrace();
+        }
+    }
+}
