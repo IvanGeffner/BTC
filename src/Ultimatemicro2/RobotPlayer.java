8acd744a9686ac569afe141d2a77de04fe6f4998
IvanGeffner <ivanegeffner@gmail.com>
2017-01-30 04:40:21 -0500

    random shooting!!

---
+++ src/Ultimatemicro2/RobotPlayer.java
@@ -0,0 +1,40 @@
+package Ultimatemicro2;
+
+import battlecode.common.GameActionException;
+import battlecode.common.RobotController;
+
+public strictfp class RobotPlayer {
+    static RobotController rc;
+
+    /**
+     * run() is the method that is called when a robot is instantiated in the Battlecode world.
+     * If this method returns, the robot dies!
+     **/
+    @SuppressWarnings("unused")
+    public static void run(RobotController rc) throws GameActionException {
+
+        // This is the RobotController object. You use it to perform actions from this robot,
+        // and to get information on its current status.
+        RobotPlayer.rc = rc;
+        switch (rc.getType()) {
+            case ARCHON:
+                Archon.run(rc);
+                break;
+            case GARDENER:
+                Gardener.run(rc);
+                break;
+            case SOLDIER:
+                Soldier.run(rc);
+                break;
+            case LUMBERJACK:
+                Lumberjack.run(rc);
+                break;
+            case TANK:
+                Tank.run(rc);
+                break;
+            case SCOUT:
+                Scout.run(rc);
+                break;
+        }
+    }
+}
